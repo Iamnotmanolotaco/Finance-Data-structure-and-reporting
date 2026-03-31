7055edb74aea845df1110d1c1543b4bf81731a2c
Iamnotmanolotaco <data.analyst7@communitylawgroup.com>
2026-03-31 10:13:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,14 +17,14 @@ from PIL import Image
 # ========== CONFIGURACIÓN DE PÁGINA ==========
 st.set_page_config(
     page_title="Procesador de Clientes | AR Collect",
-    page_icon="https://raw.githubusercontent.com/Iamnotmanolotaco/Finance-Data-structure-and-reporting/main/assets/image.png",
+    page_icon="https://raw.githubusercontent.com/Iamnotmanolotaco/Finance-Data-structure-and-reporting/main/assets/favicon4.png",
     layout="wide",
     initial_sidebar_state="expanded"
 )
 
 # ========== CONFIGURACIÓN DE IMÁGENES FIJAS ==========
-BANNER_URL = "https://raw.githubusercontent.com/Iamnotmanolotaco/Finance-Data-structure-and-reporting/main/assets/image.png"
-LOGO_URL = "https://raw.githubusercontent.com/Iamnotmanolotaco/Finance-Data-structure-and-reporting/main/assets/image.png"
+BANNER_URL = "https://raw.githubusercontent.com/Iamnotmanolotaco/Finance-Data-structure-and-reporting/main/assets/banner7.png"
+LOGO_URL = "https://raw.githubusercontent.com/Iamnotmanolotaco/Finance-Data-structure-and-reporting/main/assets/image6.png"
 
 # ========== CONTRASEÑA PARA EDITOR ==========
 EDITOR_PASSWORD = "manolotaco123"
